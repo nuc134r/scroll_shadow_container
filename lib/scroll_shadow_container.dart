acf7392ce8299710ddb45f5e68cc827cafc3c205
nuc134r <13202642+nuc134r@users.noreply.github.com>
2022-07-30 19:29:57 +0300

    2.1.1+2

--- lib/scroll_shadow_container.dart
+++ lib/scroll_shadow_container.dart
@@ -176,14 +176,15 @@ class _ScrollShadowContainerState extends State<ScrollShadowContainer> {
   @override
   void initState() {
     super.initState();
-	
-	/// This allows a value of type T or T? to be treated as a value of type T?.
-	/// Allows to get rid of pub.dev "The receiver can't be null, so the null-aware operator '?.' is unnecessary." warning.
-	T? _ambiguate<T>(T? value) => value;
-	
+
+    /// This allows a value of type T or T? to be treated as a value of type T?.
+    /// Allows to get rid of pub.dev "The receiver can't be null, so the null-aware operator '?.' is unnecessary." warning.
+    T? _ambiguate<T>(T? value) => value;
+
     // After widget is built for the first time and scroll controller
     // has a client attached we can update shadows visibility
-    _ambiguate(WidgetsBinding.instance)?.addPostFrameCallback((_) => _updateShadowsVisibility());
+    _ambiguate(WidgetsBinding.instance)
+        ?.addPostFrameCallback((_) => _updateShadowsVisibility());
   }
 
   @override
